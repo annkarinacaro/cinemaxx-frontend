f63122bf81f5c8ee02a323a84255771e3df1e347
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-12-01 10:19:04 +0100

    Files created

---
+++ pages/about-us/about-us.js
@@ -0,0 +1,8 @@
+export default () => {
+    const content = document.querySelector(".content");
+    fetch("./pages/about-us/about-us.html")
+        .then((response) => response.text())
+        .then((html) => {
+        content.innerHTML = html;
+    });
+};
