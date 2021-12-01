2fc00ea4a3d552b2de7bb8d92062f7c760356edb
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-12-01 09:54:06 +0100

    Base root

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 import renderMain from "./pages/main/main.js";
 // import renderAbout from "./pages/about/about.js";
 
-const router = new Navigo("/cinemaxx-frontend/", { hash: true });
+const router = new Navigo("/", { hash: true });
 router
   .on({
-  	"/": () => {
+  	"/cinemaxx-frontend/": () => {
       renderMain();
     },
     // "about": () => {
