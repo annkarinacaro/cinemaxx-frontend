7a29b0f40849469776584f2e4d7eeb6b55cd5c51
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-12-01 09:50:23 +0100

    main.js setup

---
+++ pages/main/main.js
@@ -0,0 +1,8 @@
+export default () => {
+    const content = document.querySelector(".content");
+    fetch("./pages/main/main.html")
+        .then((response) => response.text())
+        .then((html) => {
+        content.innerHTML = html;
+    });
+};
