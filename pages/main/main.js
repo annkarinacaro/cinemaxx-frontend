1e14e4445a90472a3f9d6dd95df807430e5a2fda
annkarinacaro <anax1410@stud.kea.dk>
2021-12-01 11:26:40 +0100

    main 2.0

--- pages/main/main.js
+++ pages/main/main.js
@@ -3,6 +3,19 @@ export default () => {
     fetch("./pages/main/main.html")
         .then((response) => response.text())
         .then((html) => {
-        content.innerHTML = html;
-    });
+            content.innerHTML = html;
+        }); 
 };
+
+let getClientLocation = () => {
+    fetch("http://www.geoplugin.net/json.gp")
+        .then((response) => response.json())
+        .then((geodata) => {
+            console.log(geodata.geoplugin_city);
+        });
+};
+
+let getLocation = () =>{
+    fetch("http://3.209.141.167/viewings/location/4?date=2021-10-25")
+    .then
+}
