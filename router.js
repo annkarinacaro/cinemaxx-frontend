f0424d13109518259b5df6960cb9b639170ecfd6
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-12-12 15:45:36 +0100

    Fixing navigo

--- router.js
+++ router.js
@@ -4,27 +4,26 @@ import renderMovie from "./pages/movie/movie.js";
 import renderEdit from "./pages/edit/edit.js";
 import renderDashboard from "./pages/dashboard/dashboard.js";
 
+
 let root = "/";
 if (location.origin.includes("github")) root = "/cinemaxx-frontend/";
 const router = new Navigo(root, { hash: true });
 router
-  .on({
-    "/": () => {
-      renderMain();
-      router.updatePageLinks();
-    },
-    "/about": () => {
-      renderAbout();
-    },
-    "/movie/:id": ({ data }) => {
-      renderMovie(data.id);
-    },
-    "/edit": () => {
-      renderEdit();
-    },
-    // :)
-    "/dashboard": () => {
-      renderDashboard();
-    },
-  })
-  .resolve();
+    .on({
+        "/": () => {
+            renderMain();
+        },
+        "about": () => {
+            renderAbout();
+        },
+        "/movie/:id/": ({ data }) => {
+            renderMovie(data.id);
+        },
+        "edit": () => {
+            renderEdit();
+        },
+        "dashboard": () => {
+            renderDashboard();
+        },
+    })
+    .resolve();
