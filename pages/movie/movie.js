42f7bb93aaeecbf429781152618b791d189d15ce
aiste334 <aistekst344@gmail.com>
2021-12-12 15:34:24 +0100

    formatting oooops

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -1,38 +1,54 @@
-import initSeatSelector, {initBookingButton} from "../../scripts/booking.js"; 
+import initSeatSelector, { initBookingButton } from "../../scripts/booking.js";
 
 export default (id) => {
-    const content = document.querySelector(".content");
-    fetch("./pages/movie/movie.html")
-        .then((response) => response.text())
-        .then((html) => {
-            content.innerHTML = html;
-            setMovieInfo(id);
-            initSeatSelector(id, initBookingButton);
-        });
+  const content = document.querySelector(".content");
+  fetch("./pages/movie/movie.html")
+    .then((response) => response.text())
+    .then((html) => {
+      content.innerHTML = html;
+      setMovieInfo(id);
+      initSeatSelector(id, initBookingButton);
+    });
 };
 
 const backendURI = "https://cinema-backend1.herokuapp.com";
 
 const setMovieInfo = (viewingId) => {
-    fetch(backendURI + "/viewing/" + viewingId)
-        .then((response) => response.json())
-        .then((viewing) => {
-            let movieTitle = document.querySelector(".movie-title");
-            let movieDescription = document.querySelector(".movie-description");
-            let movieRating = document.querySelector(".movie-rating");
-            let imagePoster = document.querySelector("img.movie-poster");
-            const movieDate = new Date(viewing.dateTime);
-            console.log(viewing.dateTime);
-            const movieTime = movieDate.getHours() + ":" + leadingZeroes(movieDate.getMinutes(), 2) + " " + movieDate.toISOString().split('T')[0];
-            movieTitle.innerHTML = "<span class='movie-time'>" + movieTime + "</span> <br>" + viewing.movie.title ;
-            movieDescription.innerHTML = viewing.movie.description + "<br><br> Cast: " + viewing.movie.actors + "<br><br> Running time: " + viewing.movie.duration + "<br><br> Age requirement: " + viewing.movie.ageRequirement;
-            movieRating.innerHTML = viewing.movie.rating + "/10 IMDb";
-            imagePoster.src = viewing.movie.poster;
-        });
+  fetch(backendURI + "/viewing/" + viewingId)
+    .then((response) => response.json())
+    .then((viewing) => {
+      let movieTitle = document.querySelector(".movie-title");
+      let movieDescription = document.querySelector(".movie-description");
+      let movieRating = document.querySelector(".movie-rating");
+      let imagePoster = document.querySelector("img.movie-poster");
+      const movieDate = new Date(viewing.dateTime);
+      console.log(viewing.dateTime);
+      const movieTime =
+        movieDate.getHours() +
+        ":" +
+        leadingZeroes(movieDate.getMinutes(), 2) +
+        " " +
+        movieDate.toISOString().split("T")[0];
+      movieTitle.innerHTML =
+        "<span class='movie-time'>" +
+        movieTime +
+        "</span> <br>" +
+        viewing.movie.title;
+      movieDescription.innerHTML =
+        viewing.movie.description +
+        "<br><br> Cast: " +
+        viewing.movie.actors +
+        "<br><br> Running time: " +
+        viewing.movie.duration +
+        "<br><br> Age requirement: " +
+        viewing.movie.ageRequirement;
+      movieRating.innerHTML = viewing.movie.rating + "/10 IMDb";
+      imagePoster.src = viewing.movie.poster;
+    });
 };
 
 function leadingZeroes(num, size) {
-    num = num.toString();
-    while (num.length < size) num = "0" + num;
-    return num;
+  num = num.toString();
+  while (num.length < size) num = "0" + num;
+  return num;
 }
--- scripts/booking.js
+++ scripts/booking.js
@@ -3,143 +3,143 @@ let viewingId;
 let bookingId;
 
 function changeColor() {
-    if (this.classList.contains("selected")) {
-        this.classList.remove("selected");
-    } else if (!this.classList.contains("booked")) {
-        this.classList.add("selected");
-    }
+  if (this.classList.contains("selected")) {
+    this.classList.remove("selected");
+  } else if (!this.classList.contains("booked")) {
+    this.classList.add("selected");
+  }
 }
 
 function getSeats(callback) {
-    fetch(backendURI + "/seats/viewing/" + viewingId)
-        .then((response) => response.json())
-        .then(callback);
+  fetch(backendURI + "/seats/viewing/" + viewingId)
+    .then((response) => response.json())
+    .then(callback);
 }
 
 function getBookingSeats(callback) {
-    fetch(backendURI + "/seats/booking/" + bookingId)
-        .then((response) => response.json())
-        .then(callback);
+  fetch(backendURI + "/seats/booking/" + bookingId)
+    .then((response) => response.json())
+    .then(callback);
 }
 
 export default function initSeatSelector(id, onSuccess) {
-    const seatSelector = document.querySelector(".seat-selector");
-    viewingId = id;
-    getSeats((seatInfo) => {
-        while (seatSelector.children.length > 0)
-            seatSelector.removeChild(seatSelector.firstChild);
-        const rowLength = seatInfo.dimensions.rows;
-        const seatLength = seatInfo.dimensions.seats;
-        const bookedSeats = seatInfo.seats;
-        for (let i = 0; i < rowLength; i++) {
-            const rowElement = document.createElement("div");
-            rowElement.classList.add("row");
-            seatSelector.appendChild(rowElement);
+  const seatSelector = document.querySelector(".seat-selector");
+  viewingId = id;
+  getSeats((seatInfo) => {
+    while (seatSelector.children.length > 0)
+      seatSelector.removeChild(seatSelector.firstChild);
+    const rowLength = seatInfo.dimensions.rows;
+    const seatLength = seatInfo.dimensions.seats;
+    const bookedSeats = seatInfo.seats;
+    for (let i = 0; i < rowLength; i++) {
+      const rowElement = document.createElement("div");
+      rowElement.classList.add("row");
+      seatSelector.appendChild(rowElement);
 
-            for (let j = 0; j < seatLength; j++) {
-                const seatElement = document.createElement("div");
-                seatElement.classList.add("seat");
-                seatElement.dataset.row = i;
-                seatElement.dataset.seat = j;
-                rowElement.appendChild(seatElement);
+      for (let j = 0; j < seatLength; j++) {
+        const seatElement = document.createElement("div");
+        seatElement.classList.add("seat");
+        seatElement.dataset.row = i;
+        seatElement.dataset.seat = j;
+        rowElement.appendChild(seatElement);
 
-                bookedSeats.forEach((bookedSeat) => {
-                    if (i === bookedSeat.row && j === bookedSeat.seat) {
-                        seatElement.classList.add("booked");
-                    }
-                });
+        bookedSeats.forEach((bookedSeat) => {
+          if (i === bookedSeat.row && j === bookedSeat.seat) {
+            seatElement.classList.add("booked");
+          }
+        });
 
-                seatElement.addEventListener("click", changeColor);
-            }
-        }
+        seatElement.addEventListener("click", changeColor);
+      }
+    }
 
-        // Screen visual
-        const screenElement = document.createElement("div");
-        screenElement.classList.add("movie-screen");
-        screenElement.innerHTML = "SCREEN";
-        seatSelector.appendChild(screenElement);
+    // Screen visual
+    const screenElement = document.createElement("div");
+    screenElement.classList.add("movie-screen");
+    screenElement.innerHTML = "SCREEN";
+    seatSelector.appendChild(screenElement);
 
-        onSuccess();
-    });
+    onSuccess();
+  });
 }
 
 export function updateBookingSeats(id) {
-    const seatSelector = document.querySelector(".seat-selector");
-    bookingId = id;
-    getBookingSeats((seatInfo) => {
-        seatInfo.seats.forEach((seat) => {
-            const seatElement = seatSelector
-                .querySelectorAll(".row")
-                [seat.row].querySelectorAll(".seat")[seat.seat];
-            seatElement.classList.remove("booked");
-            seatElement.classList.add("selected");
-        });
+  const seatSelector = document.querySelector(".seat-selector");
+  bookingId = id;
+  getBookingSeats((seatInfo) => {
+    seatInfo.seats.forEach((seat) => {
+      const seatElement = seatSelector
+        .querySelectorAll(".row")
+        [seat.row].querySelectorAll(".seat")[seat.seat];
+      seatElement.classList.remove("booked");
+      seatElement.classList.add("selected");
     });
+  });
 }
 
 export function initBookingButton() {
-    const bookingButton = document.querySelector(".book-button");
-    bookingButton.addEventListener("click", makeBooking);
+  const bookingButton = document.querySelector(".book-button");
+  bookingButton.addEventListener("click", makeBooking);
 }
 
 export function initUpdateButton(email, onSuccess) {
-    const bookingButton = document.querySelector(".update-button");
-    bookingButton.addEventListener("click", () => putBooking(email, onSuccess));
+  const bookingButton = document.querySelector(".update-button");
+  bookingButton.addEventListener("click", () => putBooking(email, onSuccess));
 }
 
 function getSelectedSeats() {
-    const seats = document.querySelectorAll(
-        ".seat-selector > .row > .seat.selected:not(.booked)"
-    );
-    const jsonSeats = [];
-    seats.forEach((seat) => {
-        const jsonSeat = { row: seat.dataset.row, seat: seat.dataset.seat };
-        jsonSeats.push(jsonSeat);
-    });
+  const seats = document.querySelectorAll(
+    ".seat-selector > .row > .seat.selected:not(.booked)"
+  );
+  const jsonSeats = [];
+  seats.forEach((seat) => {
+    const jsonSeat = { row: seat.dataset.row, seat: seat.dataset.seat };
+    jsonSeats.push(jsonSeat);
+  });
 
-    return jsonSeats;
+  return jsonSeats;
 }
 
 function putBooking(email, onSuccess) {
-    const seats = getSelectedSeats();
-    const body = {
-        viewing: viewingId,
-        email: email,
-        seats: seats,
-    };
+  const seats = getSelectedSeats();
+  const body = {
+    viewing: viewingId,
+    email: email,
+    seats: seats,
+  };
 
-    fetch(backendURI + "/booking/" + bookingId, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-    })
-        .then((response) => response.json())
-        .then((booking) => {
-            onSuccess();
-        });
+  fetch(backendURI + "/booking/" + bookingId, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((booking) => {
+      onSuccess();
+    });
 }
 
 function makeBooking() {
-    const email = document.querySelector(".email").value;
-    const seats = getSelectedSeats();
-    const body = {
-        viewing: viewingId,
-        email: email,
-        seats: seats,
-    };
+  const email = document.querySelector(".email").value;
+  const seats = getSelectedSeats();
+  const body = {
+    viewing: viewingId,
+    email: email,
+    seats: seats,
+  };
 
-    fetch(backendURI + "/booking", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-    })
-        .then((response) => response.json())
-        .then((booking) => {
-            location.reload();
-            console.log("amaze: ", booking);
-        });
+  fetch(backendURI + "/booking", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((booking) => {
+      location.reload();
+      console.log("amaze: ", booking);
+    });
 }
