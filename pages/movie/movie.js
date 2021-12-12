87f3b3b353863733c7af06f720f2a8418782a2d4
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-12-12 13:36:57 +0100

    Navigo routing fix for github pages

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -21,10 +21,18 @@ const setMovieInfo = (viewingId) => {
             let movieDescription = document.querySelector(".movie-description");
             let movieRating = document.querySelector(".movie-rating");
             let imagePoster = document.querySelector("img.movie-poster");
-            movieTitle.innerHTML = viewing.movie.title;
-            movieDescription.innerHTML = viewing.movie.description;
-            console.log(viewing);
-            movieRating.innerHTML = viewing.movie.rating;
+            const movieDate = new Date(viewing.dateTime);
+            console.log(viewing.dateTime);
+            const movieTime = movieDate.getHours() + ":" + leadingZeroes(movieDate.getMinutes(), 2) + " " + movieDate.toISOString().split('T')[0];
+            movieTitle.innerHTML = "<span class='movie-time'>" + movieTime + "</span> " + viewing.movie.title ;
+            movieDescription.innerHTML = viewing.movie.description + "<br><br> Cast: " + viewing.movie.actors + "<br><br> Running time: " + viewing.movie.duration + "<br><br> Age requirement: " + viewing.movie.ageRequirement;
+            movieRating.innerHTML = viewing.movie.rating + "/10 IMDb";
             imagePoster.src = viewing.movie.poster;
         });
 };
+
+function leadingZeroes(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+}
